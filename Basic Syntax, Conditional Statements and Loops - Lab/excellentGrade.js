69f16f45b0ba5ac167de737fb0917b0dbedd677f
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-04-15 14:47:15 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/excellentGrade.js
@@ -0,0 +1,20 @@
+function isExellent(grade) {
+    if (grade >= 5.50) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+
+
+
+
+or
+
+
+
+
+function isExellent(grade) {
+    console.log(grade >= 5.50 ? "Excellent" : "Not excellent");
+    }
