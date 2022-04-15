f17490466846288cf539df31ec3fa2e43958b7aa
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-04-15 14:47:28 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/foreignLangunges.js
@@ -0,0 +1,21 @@
+function languange(country) {
+
+    if (country === 'USA') {
+        console.log("English");
+    }
+    else if (country === 'Mexico') {
+        console.log("Spanish");
+    }
+    else if (country === 'Spain') {
+        console.log("Spanish");
+    }
+    else if (country === 'Argentina') {
+        console.log("Spanish");
+    }
+    else if (country == 'England') {
+        console.log("English");
+    }
+    else {
+        console.log("unknown")
+    }
+}
