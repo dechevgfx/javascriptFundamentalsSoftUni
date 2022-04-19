f8d473b9f5abdebcabb321b4ac881961c1df088a
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-04-19 10:47:59 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/amazingNumbers.js
@@ -0,0 +1,12 @@
+function amazingNumber(number){
+    number = number.toString()
+    let sum = 0
+    for (let i = 0; i < number.length; i++){
+        sum += Number(number[i])
+    }
+    let result = sum.toString().includes('9');
+    console.log(result?
+        `${number} Amazing? True` :
+        `${number} Amazing? False`);
+}
+amazingNumber(1233)
