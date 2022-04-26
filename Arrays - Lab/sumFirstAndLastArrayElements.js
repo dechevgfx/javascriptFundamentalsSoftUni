e53ff19905db76a2a49f32bf998ad4b814c3f8de
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-04-26 20:46:11 +0300

    Add files via upload

---
+++ Arrays - Lab/sumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLast(input) {
+    let firstElement = input[0];
+    let lastElement = input[input.length - 1]
+    let result = firstElement + lastElement
+    console.log(result)
+}
+sumFirstAndLast([20, 30, 40])
