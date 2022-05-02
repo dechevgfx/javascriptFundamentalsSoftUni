bf49041a76791c01ca21abb6d6edecbff99ea709
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-05-02 22:00:43 +0300

    Add files via upload

---
+++ Functions - Lab/orders.js
@@ -0,0 +1,22 @@
+function coffeeShop(product, quantity) {
+    let price = 0
+    switch (product) {
+        case "coffee":
+price = quantity * 1.50
+            break;
+
+        case "water":
+price = quantity * 1.00
+            break;
+
+        case "coke":
+price = quantity * 1.40
+            break;
+
+        case "snacks":
+price = quantity * 2.00
+            break;
+    }
+    console.log(price.toFixed(2))
+}
+coffeeShop("water", 5)
