56a7c56de66f3393f7e24006470758000b9b40d9
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-05-05 16:24:36 +0300

    Add files via upload

---
+++ Functions - Exercise/nXnMatrix.js
@@ -0,0 +1,11 @@
+function matix(number) {
+    let output = ""
+    for (let i = 0; i < number; i++) {
+        output += `${number} `
+    }
+    for (let i = 0; i < number; i++) {
+        console.log(output)
+    }
+
+}
+matix(5)
