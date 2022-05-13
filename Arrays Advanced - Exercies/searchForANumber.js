983a329ba6b2de7c35eca565d3874e340b6ba88f
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-05-13 21:18:23 +0300

    Add files via upload

---
+++ Arrays Advanced - Exercies/searchForANumber.js
@@ -0,0 +1,17 @@
+function solve(arr, command){
+
+    let counter = 0;
+    let firstArr = arr.slice(0, command[0]);
+    let secondArr = firstArr.splice(0, command[1]);
+    let thirdArr = firstArr.filter(isContainsNum);
+
+    function isContainsNum(num){
+        return num === command[2];
+    }
+
+    for(let i = 0; i < thirdArr.length; i++){
+        counter++
+    }
+
+    console.log(`Number ${command[2]} occurs ${counter} times.`);
+}
