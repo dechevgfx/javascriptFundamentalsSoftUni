033ca1cb5ec634a4752b8dd8cd107a4cae92c541
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-05-13 21:18:55 +0300

    Add files via upload

---
+++ Arrays Advanced - Exercies/sortAnArrayBy2Criteria.js
@@ -0,0 +1,15 @@
+function solve(arr){
+
+    let sorted = arr.sort(sortNames);
+    console.log(sorted.join('\n'));
+
+    function sortNames(a, b) {
+        let firstCriteria = a.length - b.length;
+
+        if(firstCriteria === 0) {
+            return a.localeCompare(b);
+        }
+
+        return firstCriteria;
+    }
+}
