57bd898d1c43bb85884b0d930ad33655283f7f56
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-05-25 14:16:25 +0300

    Add files via upload

---
+++ Object and Clasess - Exercies/employees.js
@@ -0,0 +1,13 @@
+function solve(input){
+
+    for(let i = 0; i < input.length; i++){
+        let name = input[i];
+        let person = {
+            name: name
+        }
+        let entries = Object.entries(person); //Returns array of all properties and their values of an object
+            for(let pair of entries){
+            console.log(`Name: ${pair[1]} -- Personal Number: ${pair[1].length}`);
+            }
+    }
+}
