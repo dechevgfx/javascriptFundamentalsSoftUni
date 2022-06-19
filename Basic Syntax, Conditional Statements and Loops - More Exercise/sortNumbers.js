cf69eb1a7fc47fe55fe81b9f3f3a598adf49afa7
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-06-19 20:11:10 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/sortNumbers.js
@@ -0,0 +1,52 @@
+// function sort(a, b, c) {
+
+
+//     if ((a >= b) && (a >= c)) {
+//         if (b >= c) {
+//             console.log(a);
+//             console.log(b);
+//             console.log(c);
+//         }
+//         else {
+//             console.log(a);
+//             console.log(c);
+//             console.log(b);
+//         }
+//     }
+//     else if ((b >= a) && (b >= c)) {
+//         if (a >= c) {
+//             console.log(b);
+//             console.log(a);
+//             console.log(c);
+//         }
+//         else {
+//             console.log(b);
+//             console.log(c);
+//             console.log(a);
+//         }
+//     }
+//     else if ((c >= a) && (c >= b)) {
+//         if (a >= b) {
+//             console.log(c);
+//             console.log(a);
+//             console.log(b);
+//         }
+//         else {
+//             console.log(c);
+//             console.log(b);
+//             console.log(a);
+//         }
+//     }
+// }
+
+// sort(['3', '1', '-2'])
+
+
+
+function sorting(a, b, c) {
+    let array = [a, b, c];
+    let result = array.sort((a,b) => b-a)
+    console.log(result.join("\n"))
+}
+
+sorting(3, 1, -2)
