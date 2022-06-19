51b44921d3adcd7508c3c613a2efea475a614f7a
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-06-19 20:10:44 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/nextDay.js
@@ -0,0 +1,14 @@
+function solve(inputYear, inputMonth, inputDay){
+    let year = Number(inputYear);
+    let month = Number(inputMonth) - 1;
+    let day = Number(inputDay);
+
+    let date = new Date(year, month, day)
+    let nextDate = new Date(date);
+    nextDate.setDate(date.getDate() + 1)
+    let outputYear = nextDate.getFullYear();
+    let outputMonth = nextDate.getMonth() + 1;
+    let outputDay = nextDate.getDate();
+    console.log(`${outputYear}-${outputMonth}-${outputDay}`)
+
+}
